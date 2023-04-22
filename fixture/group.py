75c9f8725633b51c170a46915f75ae92e83635e9
Marina18241 <9mariam7@gmail.com>
2023-04-22 22:32:04 +0300

    Реализовано удаление группы
    Реализованы тесты для создания и удаления групп в адресной книге с графическим интерфейсом Windows

--- fixture/group.py
+++ fixture/group.py
@@ -15,6 +15,18 @@ class GroupHelper:
         self.close_group_editor()
         return group_list
 
+    def get_group_nodes(self):
+        tree = self.group_editor.window(auto_id="uxAddressTreeView")
+        root = tree.tree_root()
+        return root.children()
+
+    def delete_group(self):
+        self.group_editor.window(auto_id="uxDeleteAddressButton").click()
+        delete_dialog = self.app.application.window(title="Delete group")
+        delete_dialog.wait("visible")
+        delete_dialog.window(auto_id="uxOKAddressButton").click()
+
+
     def add_new_group(self, name):
         self.open_group_editor()
         self.group_editor.window(auto_id="uxNewAddressButton").click()
--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -1,4 +1,23 @@
+import random
 
 
 def test_delete_group(app):
-    pass
+    group_list = app.groups.get_group_list()
+    if len(group_list) == 1:
+        app.groups.add_new_group("test2")
+        group_list = app.groups.get_group_list()
+    app.groups.open_group_editor()
+    nodes = app.groups.get_group_nodes()
+    random_node = random.choice(nodes)
+    random_node.click()
+    app.groups.delete_group()
+    new_group_list=app.groups.get_group_list()
+    assert len(group_list)==len(new_group_list)+1
+
+
+
+
+
+
+
+
